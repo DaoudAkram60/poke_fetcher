2d10f1d191b0f2b7206b5a8799f9b5276ee8bf27
Le Akram <136912477+DaoudAkram60@users.noreply.github.com>
2024-09-07 15:01:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 async function pokeFetch(){
     const name = document.getElementById("input").value.toLowerCase();
+    if(name==''){
+        window.alert("Please Name a Pokemon");
+        console.error("user did not name a pokemon")
+    }
     const sprite = document.getElementById("sprite");
     //console.log(name);
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    //console.log(response)
+    console.log(response)
+    if(response.ok!=true && name!=''){
+        window.alert("Check Your Pokemon's Name")
+        console.error("user did not name a correct pokemon");
+    }
     const data = await response.json();
     //console.log(data)
     const spriteUrl = data.sprites.other.home.front_default;
@@ -18,3 +26,5 @@ async function pokeFetch(){
     //console.log(spriteUrl);
     sprite.src = spriteUrl;
 }
+
+
